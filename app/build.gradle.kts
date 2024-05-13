74dd485f27dc9cd6510c6328aef613e3788ac0a7
lisiyang <lisiyang44@foxmail.com>
2024-05-13 23:40:34 +0800

    增加签名配置和尤里卡图标

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,9 +1,17 @@
+import org.jetbrains.kotlin.gradle.plugin.mpp.pm20.util.archivesName
+import java.util.Properties
+import java.io.FileInputStream
+
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
     id("kotlin-kapt")
     id("kotlin-parcelize")
 }
+
+val keystorePropertiesFile = rootProject.file("keystore.properties")
+val keystoreProperties = Properties()
+keystoreProperties.load(FileInputStream(keystorePropertiesFile))
 
 android {
     namespace = "com.lames.standard"
@@ -15,7 +23,7 @@ android {
         targetSdk = 33
         versionCode = 1
         versionName = "1.0"
-
+        setProperty("archivesBaseName", "LamesApp_${versionName}_${versionCode}")
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
@@ -25,6 +33,7 @@ android {
     }
 
     buildTypes {
+        debug { signingConfig = null }
         release {
             isMinifyEnabled = false
             proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
@@ -37,6 +46,22 @@ android {
     kotlinOptions {
         jvmTarget = "1.8"
     }
+    signingConfigs {
+        create("lames") {
+            storeFile = file(keystoreProperties.getProperty("storeFile"))
+            storePassword = keystoreProperties.getProperty("storePassword")
+            keyAlias = keystoreProperties.getProperty("keyAlias")
+            keyPassword = keystoreProperties.getProperty("keyPassword")
+        }
+    }
+    flavorDimensions += "api"
+    productFlavors {
+        create("standard") {
+            dimension = "api"
+            signingConfig = signingConfigs.getByName("lames")
+        }
+    }
+
 }
 
 dependencies {
